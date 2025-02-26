8788389fab87be0785470745e01f68c58d99bb66
u <u>
2025-02-26 14:43:41 +0300

    update

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ name="jinja2-easy.generator"
 cd *"$name"
 python3 -m build -s -n -o .
 py2pack generate "$name" --localfile ./jinja2_easy.generator.egg-info/PKG-INFO --source-glob '%{name}-%{version}.tar.gz'
-sed -i "s~^CHOOSE:.*~~; s~%{python_sitelib}/jinja2-easy\.generator.*~~; s~%files\(.*\)~%files \1\n%{python_sitelib}/jinja2_easy/\n%{python_sitelib}/jinja2_easy.generator-%{version}.dist-info/~; s/\(%autosetup.*\)jinja2-easy.generator\(.*\)/\1jinja2_easy_generator\2/g;" *.spec
+sed -i "s~%{pypi_name}-%{version}~jinja2_easy_generator-%{version}~; s~^CHOOSE:.*~~; s~%{python_sitelib}/jinja2-easy\.generator.*~~; s~%files\(.*\)~%files \1\n%{python_sitelib}/jinja2_easy/\n%{python_sitelib}/jinja2_easy.generator-%{version}.dist-info/~; s/\(%autosetup.*\)jinja2-easy.generator\(.*\)/\1jinja2_easy_generator\2/g;" *.spec
 )
 mv *"$name"/*.{spec,tar.gz} "$outdir"/
 
